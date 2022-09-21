21c2f781220f5e4de58f5cbda769b92342f07ce7
quocbld <quocbldfg@gmail.com>
2022-09-21 09:42:07 +0700

    Code bo sung-LE BA QUOC

--- app/src/main/java/com/example/quoc_2050531200260/Main2Activity.java
+++ app/src/main/java/com/example/quoc_2050531200260/Main2Activity.java
@@ -2,13 +2,24 @@ package com.example.quoc_2050531200260;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
